7b60160ac4c834ee04605ad32876386d36fd1b41
jyotimishra05 <jyotihra28@gmail.com>
2024-02-29 23:22:30 +0530

    created reduxStore with userSlice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import "./index.css"
 import Body from './components/Body'
+import { Provider } from 'react-redux'
+import { store } from './utils/appStore'
 
 const App = () => {
   return (
-    <div >
-  <Body/>
-      
+    <div>
+      <Provider store={store}>
+        <Body />
+      </Provider>
     </div>
-  )
+  );
 }
 
 export default App
